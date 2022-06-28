4e975a35162b9f1db8286b31d2cf6660277f0d06
h1kupz <h1kupz3z@gmail.com>
2022-06-28 23:18:23 +0700

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   target: "static",
-  router: {
-    base: "/dist/",
-  },
   build: {
     postcss: {
       postcssOptions: {
