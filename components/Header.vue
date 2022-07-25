007b4d88c87f5ff122a1f493cd4b455f0ce14513
tm-ox <timothy@oxenfree.net>
2022-07-25 21:51:25 +0700

    Extra Link + Page Margins

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,9 @@
 <template>
-  <header class="z-50">
-    <a href="#home" class="flex w-8 h-8 p-0 rounded-full bg-cover">
-      <Logo />
-    </a>
+  <header>
+    <div class="w-full max-w-screen-2xl mx-auto">
+      <a href="#home" class="flex w-8 h-8 p-0 rounded-full bg-cover">
+        <Logo />
+      </a>
+    </div>
   </header>
 </template>
--- components/Work.vue
+++ components/Work.vue
@@ -7,7 +7,7 @@
       <div class="flex flex-col">
         <h1 class="mb-2">
           slow build.<br />
-          here is a pdf for now..
+          here is a pdf for <a href="#contact">now</a>..
         </h1>
         <a href="TM-OX-Folio-0722-Web.pdf" target="_blank" class="">FOLIO</a>
       </div>
